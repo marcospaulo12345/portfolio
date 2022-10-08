be437c4ad2109a45a20527ce4d86dac83fbae5a1
marcospaulo12345 <marcospaulo.mpf34@gmail.com>
2022-10-08 15:46:03 -0300

    Iniciado a criação da página home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,12 +4,29 @@ import Header from "../../components/Header";
 
 import "./styles.css";
 
+import DevelopImg from '../../assets/images/developer.svg';
+import DivPageImg from '../../assets/images/div-page.png';
 
 export default function Home() {
     return (
         <div className="container-home">
             <Header />
-            Home
+            <main>
+                <div className="home-about">
+                    <div className="home-about-left">
+                        <p className="area">Desenvolvedor WEB</p>
+                        <h1>Marcos Paulo</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <button>Baixar Currículo</button>
+                    </div>
+                    <div className="home-about-right">
+                        <img src={DevelopImg} />
+                    </div>
+                </div>
+                <div className="div-page">
+                    <img src={DivPageImg}/>
+                </div>
+            </main>
         </div>
     )
 }
