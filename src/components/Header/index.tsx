22e014033fd0f1e695e1119be5f76ee851daed18
marcospaulo12345 <marcospaulo.mpf34@gmail.com>
2022-10-14 21:13:28 -0300

    criado a animação da navbar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-import './styles.css'
+import MenuBurger from '../../assets/images/menu.png';
+
+import './styles.css';
 
 export default function Headers() {
+    const [openMenu, setOpenMenu] = useState(false);
+    console.log(openMenu)
+
     return (
         <header className="container-header">
-            <div>
+            <div 
+                className={openMenu ? "is-open-overlayer navbar-overlay": "navbar-overlay"}
+                onClick={() => setOpenMenu(!openMenu)}
+            ></div>
+            <button type="button" className="nav-burger" onClick={() => setOpenMenu(!openMenu)}>
+                <img src={MenuBurger} width="32" height="32"/>
+            </button>
+            <div className="logo">
                 <h1> <span>&lt;/</span> Marcos Paulo <span>&gt;</span> </h1>
             </div>
 
-            <ul>
+            <ul className={openMenu ? "is-open-menu": "menu"}>
                 <li>
                     <Link to='/'>
                         Home
